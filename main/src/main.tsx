2842d55ddc02c79317a9fb11117188e4c2890a96
Ivan Atias <ivan.d.atias@gmail.com>
2024-07-26 15:15:26 -0400

    👀 chore: apply formatting

--- main/src/main.tsx
+++ main/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 
 // biome-ignore lint/style/noNonNullAssertion: <just ignore>
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<ManuallyTestInHere />
-	</React.StrictMode>
+  <React.StrictMode>
+    <ManuallyTestInHere />
+  </React.StrictMode>
 )
--- main/vitest.config.ts
+++ main/vitest.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: 'happy-dom',
-		setupFiles: './src/__tests__/setup.ts'
-	}
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './src/__tests__/setup.ts'
+  }
 })
