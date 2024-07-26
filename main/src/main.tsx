1116174de1eb217fef1413e64277ba15519c36cd
Ivan Atias <ivan.d.atias@gmail.com>
2024-07-26 00:31:17 -0400

    👀 chore: rename manual testing file

--- main/src/main.tsx
+++ main/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ManuallyTestInHere } from './App.tsx'
+import { ManuallyTestInHere } from './manually-test-here.tsx'
 import './index.css'
 
 // biome-ignore lint/style/noNonNullAssertion: <just ignore>
